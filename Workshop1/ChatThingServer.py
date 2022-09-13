46f898816c4bc612e050616f44f476b0875a2eb6
ChineseWarlord <ChineseWarlord@github.com>
2022-09-13 12:00:50 +0200

    Added stuff

---
+++ Workshop1/ChatThingServer.py
@@ -0,0 +1,13 @@
+"""
+Project 1: encrypted chat
+
+Design a secure chat system, with the following features:
+
+● A separate server that only handles key and message exchange
+● Encrypted group chats
+● Persistent chat logs that are loaded if you close and reopen the program
+
+You can assume the server can be trusted for the key exchange, but it should not
+be able to decode messages
+"""
+
