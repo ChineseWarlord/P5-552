7cd999c2971d73bff066cab3bc3a8c068b5bf56b
ChineseWarlord <ChineseWarlord@github.com>
2022-09-29 14:55:55 +0200

    Works

--- Lecture 7/tcp_socket_client.py
+++ Lecture 7/tcp_socket_client.py
@@ -2,8 +2,14 @@ import socket
 
 HOST = '127.0.0.1'
 PORT = 5000
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST,PORT))
-    s.sendall(b'Hello world')
-    data = s.recv(1024)
-print('Received', repr(data))
+    while True:
+        s.sendall(input("Enter text: \n").encode())
+        data = s.recv(1024)
+        print('Received:', repr(data.decode()))
+        
+        if data.decode() == "Closing connection ...":
+            break
+        
--- Lecture 7/tcp_socket_server.py
+++ Lecture 7/tcp_socket_server.py
@@ -1,17 +1,38 @@
 import socket
+from time import sleep
+import select
+
+
 HOST = '127.0.0.1'
 PORT = 5000                 # An integer from 1 to 65535
+
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:    # AF_INET is the Internet address family for IPv4
     s.bind((HOST, PORT))
     s.listen()
-    print("Server is waiting for a client ...")                                                  
+    print("Server is waiting for a client ...")                                                 
+    #s.settimeout(5)
+    
     conn, addr = s.accept()
     with conn:
         print('Connected by', addr)
         while True:
             data = conn.recv(1024)                  # 1024 is the maximum amount of bytes to be received at once
-            print("Server is receving data ...")
-            if not data:
+            
+            print('Received: ', data.decode())
+            
+            
+            #if data:
+            #    print("Server is receving data ...")
+                
+            #if not data:
+            #    break
+            data2 = "Closing connection ..."
+            if data.decode() == "Close connection":
+                conn.sendall(data2.encode())
+                s.close()
                 break
-            conn.sendall(data)
+            else:
+                conn.sendall(data)
 # No s.close is needed
