814f58e29c738f3a6f119da563c33c8c6fc4f691
ChineseWarlord <ChineseWarlord@github.com>
2022-10-11 09:29:55 +0200

    Dunno what I changed

--- Lecture 7/tcp_socket_server.py
+++ Lecture 7/tcp_socket_server.py
@@ -1,38 +1,39 @@
 import socket
-from time import sleep
+import time
 import select
+from threading import Thread
 
-
-HOST = '192.168.31.17'
+HOST = '192.168.31.66'
 PORT = 5000                 # An integer from 1 to 65535
 
-
+def recvmsg(conn):
+     data = conn.recv(1024)    # 1024 is the maximum amount of bytes to be received at once
+     print('Received: ', data.decode())
+     return data.decode()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:    # AF_INET is the Internet address family for IPv4
     s.bind((HOST, PORT))
     s.listen()
-    print("Server is waiting for a client ...")                                                 
-    #s.settimeout(5)
-    
+    print("Server is waiting for a client ...")
     conn, addr = s.accept()
+    s.setblocking(False)
+    
     with conn:
         print('Connected by', addr)
+        
+        #t1 = Thread(target=recvmsg(conn))
+        #t1.start()
         while True:
-            data = conn.recv(1024)                  # 1024 is the maximum amount of bytes to be received at once
-            
-            print('Received: ', data.decode())
-            
-            
-            #if data:
-            #    print("Server is receving data ...")
-                
-            #if not data:
-            #    break
-            data2 = "Closing connection ..."
-            if data.decode() == "Close connection":
-                conn.sendall(data2.encode())
-                s.close()
-                break
+            readmsg = select.select([conn], [], [], 10)
+            if (readmsg):
+                recvmsg(conn)
+                print("Received msg \n")
             else:
-                conn.sendall(input("Enter text: \n".encode()))
-# No s.close is needed
+                data2 = "Closing connection ..."
+            
+                if recvmsg == "Close connection":
+                    conn.sendall(data2.encode())
+                    s.close()
+                    break
+                else:
+                    conn.sendall(input("Enter text: \n").encode())
